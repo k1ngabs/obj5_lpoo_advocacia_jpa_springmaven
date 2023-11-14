3be372c4d16ee59674c19634526c5ce1df43e49c
Gabriel Carvalho <69277918+k1ngabs@users.noreply.github.com>
2023-11-14 02:05:20 -0300

    none

---
+++ src/main/java/br/edu/ifsul/cstsi/obj5_lpoo_advocacia_jpa_springmaven/Obj5LpooAdvocaciaJpaSpringmavenApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifsul.cstsi.obj5_lpoo_advocacia_jpa_springmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Obj5LpooAdvocaciaJpaSpringmavenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Obj5LpooAdvocaciaJpaSpringmavenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/edu/ifsul/cstsi/obj5_lpoo_advocacia_jpa_springmaven/Obj5LpooAdvocaciaJpaSpringmavenApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifsul.cstsi.obj5_lpoo_advocacia_jpa_springmaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Obj5LpooAdvocaciaJpaSpringmavenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
