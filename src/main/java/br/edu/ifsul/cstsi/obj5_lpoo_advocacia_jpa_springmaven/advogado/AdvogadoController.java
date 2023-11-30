d4a41cf6464a2dce4c20cc4fbdcd7f93fac9f3bf
Aluno IFSUL <aluno@email.com>
2023-11-30 16:28:49 -0300

    obj6 em aula

--- src/main/java/br/edu/ifsul/cstsi/obj5_lpoo_advocacia_jpa_springmaven/advogado/AdvogadoController.java
+++ src/main/java/br/edu/ifsul/cstsi/obj5_lpoo_advocacia_jpa_springmaven/advogado/AdvogadoController.java
@@ -1,5 +1,7 @@
 package br.edu.ifsul.cstsi.obj5_lpoo_advocacia_jpa_springmaven.advogado;
 
+import br.edu.ifsul.cstsi.obj5_lpoo_advocacia_jpa_springmaven.pessoa.PessoaController;
+import br.edu.ifsul.cstsi.obj5_lpoo_advocacia_jpa_springmaven.processo.ProcessoController;
 import org.springframework.core.SpringVersion;
 import org.springframework.stereotype.Controller;
 
@@ -10,6 +12,38 @@ public class AdvogadoController {
     private static final Scanner input = new Scanner(System.in);
     private static AdvogadoService advogadoService;
     public static void main(String[] args) {
-
+        int opcao;
+        do {
+            System.out.print("\n-------  Home -------");
+            System.out.print(
+                    """
+                        1. Inserir Advogado
+                        2. Visualizar Advogado
+                        3. Atualizar Advogado
+                        4. Deletar Advogado
+                        Opção (Zero p/sair):\s""");
+            opcao = input.nextInt();
+            input.nextLine();
+            switch (opcao) {
+                case 1 -> AdvogadoController.main(null);
+                case 2 ->{System.out.print("\n-------  Home -------");
+                            System.out.print(
+                                """
+                                1. Inserir Advogado
+                                2. Visualizar Advogado
+                                3. Atualizar Advogado
+                                4. Deletar Advogado
+                                Opção (Zero p/sair):\s""");
+                            opcao = input.nextInt();
+                            input.nextLine();
+                }
+                case 3 -> ProcessoController.main(null);
+                default -> {
+                    if (opcao != 0) System.out.println("Opção inválida.");
+                }
+            }
+        } while(opcao != 0);
+        System.out.println("\n    Operacao finalizada");
+        input.close();
     }
 }
